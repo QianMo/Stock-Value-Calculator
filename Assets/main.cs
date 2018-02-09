3130ab64c583069565a751e045f4b18815fd4e28
浅墨 <happylifemxy@163.com>
2018-02-09 10:28:57 +0800

    加入过夜风控比率的计算
    加入过夜风控比率的计算

--- Assets/main.cs
+++ Assets/main.cs
@@ -11,9 +11,11 @@ using System;
 
 public class main : MonoBehaviour
 {
+    //股价
     public Text Price = null;
     private float PriceValue = 0;
 
+    //股数
     public Text Number = null;
     private float NumberValue = 0;
 
@@ -30,6 +32,20 @@ public class main : MonoBehaviour
 
     //抵押比例
     public float MortgageRatio = 0.5f;
+    //该股票抵押比率(风控使用)
+    const float STOCK_MORTGAGE_RATIO = 0.55f;
+
+    [Header("过夜风控比率")]
+    public Text OvernightRiskControlRatio = null;
+    public float OvernightRiskControlRatioValue = 0;
+
+    [Header("保证金要求")]
+    public Text GuaranteeRequire = null;
+    public float GuaranteeRequireValue = 0;
+
+    [Header("持有该股票市值")]
+    public Text StockMarketCapitalization = null;
+    public float StockMarketCapitalizationValue = 0;
 
     [Header("总市值")]
     public Text TotalMarketCapitalization = null;
@@ -74,10 +90,65 @@ public class main : MonoBehaviour
         return TotalMarketCapitalizationValue;
     }
 
+    //--------------------------------------------------------
+    /// 计算持有该股票市值
+    //--------------------------------------------------------
+    float GetStockMarketCapitalization()
+    {
+        //持有该股票市值
+        StockMarketCapitalizationValue = PriceValue * NumberValue;
+
+        StockMarketCapitalization.text = StockMarketCapitalizationValue.ToString();
+
+        return StockMarketCapitalizationValue;
+    }
+
+
+    //--------------------------------------------------------
+    /// 计算保证金要求
+    //--------------------------------------------------------
+    float GetGuaranteeRequire()
+    {
+        //保证金要求=持有股票市值 * 该股票抵押比率
+        GuaranteeRequireValue = StockMarketCapitalizationValue * STOCK_MORTGAGE_RATIO;
+
+        GuaranteeRequire.text = GuaranteeRequireValue.ToString();
+
+        return GuaranteeRequireValue;
+    }
+
+    //--------------------------------------------------------
+    /// 计算过夜风控比率
+    //--------------------------------------------------------
+    float GetOvernightRiskControlRatio()
+    {
+
+        //过夜风控比率=1-保证金要求/资产总市值
+        OvernightRiskControlRatioValue = (1 - GuaranteeRequireValue / TotalMarketCapitalizationValue) * 100;
+
+        OvernightRiskControlRatio.text = OvernightRiskControlRatioValue.ToString() + "%";
+
+        return OvernightRiskControlRatioValue;
+    }
+
+
+
     void Update()
     {
+
+        //计算持有该股票市值
+        GetStockMarketCapitalization();
+
+        //计算保证金要求
+        GetGuaranteeRequire();
+
+        //计算过夜风控比率
+        GetOvernightRiskControlRatio();
+
+        //计算保证金比率
         GetGuaranteeRatioValue();
 
+        //计算总市值
         GetTotalMarketCapitalization();
     }
 
